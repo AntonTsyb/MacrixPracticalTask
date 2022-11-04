7d95fdb0042f3fd4dcf2dc0be2dcb9e301822c79
Anton Tsyb <antontsyb@gmail.com>
2022-11-04 14:56:45 +0100

    Add base model, interface and service Also add DataGrid

---
+++ AntonTsyb-Macrix/Models/PersonModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MacrixPracticalTask.Models
+{
+    public class PersonModel
+    {
+        public PersonModel(string firstName,
+            string lastName, 
+            string phoneNumber,
+            DateTime dateOfBirth,
+            int age,
+            AddressModel address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            DateOfBirth = dateOfBirth;
+            Age = age;
+            Address = address;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age { get; }
+
+        public AddressModel Address { get; set; }
+    }
+}
---
+++ AntonTsyb-Macrix/Services/XmlFileService.cs
@@ -0,0 +1,37 @@
+﻿using MacrixPracticalTask.Models;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace MacrixPracticalTask.Services
+{
+    public class XmlFileService : IXmlFileService
+    {
+        private readonly XmlSerializer _xmlSerializer;
+
+        private const string XmlFileName = "DataToLoad.xml";
+
+        public XmlFileService()
+        {
+            _xmlSerializer = new XmlSerializer(typeof(ObservableCollection<PersonModel>));
+        }
+
+        public ObservableCollection<PersonModel>? LoadData()
+        {
+            var data = new ObservableCollection<PersonModel>();
+            // Load data from xml file
+            using FileStream fs = new(XmlFileName, FileMode.OpenOrCreate, FileAccess.Read);
+            if (fs != null && fs.Length > 0)
+                data = (ObservableCollection<PersonModel>?)_xmlSerializer.Deserialize(fs);
+
+            return data;
+        }
+
+        public void SaveData(ObservableCollection<PersonModel> dataToSave)
+        {
+            // Re-write all data in file to new from DataGrid
+            using FileStream fs = new(XmlFileName, FileMode.Truncate, FileAccess.Write);
+            _xmlSerializer.Serialize(fs, dataToSave);
+        }
+    }
+}

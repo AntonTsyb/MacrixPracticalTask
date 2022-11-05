2935f472bd88112b8308d08fb3e6bd786f40380c
AntonTsyb <47610787+AntonTsyb@users.noreply.github.com>
2022-11-05 12:36:17 +0100

    Change model to add new data (#2)
    Add validation

--- AntonTsyb-Macrix/MainWindow.xaml.cs
+++ AntonTsyb-Macrix/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using MacrixPracticalTask.Models;
+using MacrixPracticalTask.Services;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace MacrixPracticalTask
 {
@@ -7,9 +13,24 @@ namespace MacrixPracticalTask
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<PersonModel> _collection;
+        private XmlFileService _xmlFileService;
         public MainWindow()
         {
             InitializeComponent();
+            _xmlFileService = new XmlFileService();
+            _collection = _xmlFileService.LoadData() ?? new ObservableCollection<PersonModel>();
+            mainGrid.ItemsSource = _collection;
         }
+
+        #region Events
+        private void mainGrid_RowEditEnding(object sender, DataGridRowEditEndingEventArgs e)
+        {
+        }
+
+        private void mainGrid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+        }
+        #endregion
     }
 }
